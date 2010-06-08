179c246698e4ad969b54cd0eeb8920bdd1c335f4
Kaithos <matthewsbrubaker@gmail.com>
2010-06-08 10:37:42 -0500

    Fixing IIS6Task.

---
+++ product/dropkick.tests/Tasks/Iis/NewVirtualIis6DirTest.cs
@@ -0,0 +1,39 @@
+﻿namespace dropkick.tests.Tasks.Iis
+{
+    using System;
+    using dropkick.Tasks.Iis;
+    using Microsoft.Web.Administration;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class NewVirtualIis6DirTest
+    {
+        [Test, Explicit]
+        public void Create_A_VirtualDiretory()
+        {
+            var task = new Iis6Task
+                           {
+                               PathOnServer = "E:\\FHLBApp\\FHLBank.Security.Web",
+                               ServerName = "srvtest19",
+                               VdirPath = "Test_dk",
+                               WebsiteName = "Exchequer",
+                               AppPoolName = "Test_dk"
+                           };
+
+            var output = task.Execute();
+            foreach (var item in output.Results)
+            {
+                Console.WriteLine(item.Message);
+            }
+        }
+
+        [Test, Explicit]
+        public void Create_An_AppPool()
+        {
+            var iis = ServerManager.OpenRemote("SrvTestWeb01");
+            iis.ApplicationPools.Add("MATTYB");
+
+            iis.CommitChanges();
+        }
+    }
+}
