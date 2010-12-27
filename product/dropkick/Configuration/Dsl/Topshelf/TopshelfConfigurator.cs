161cad2b1456869b920b45417afbec47137c5a70
Kaithos <matthewsbrubaker@gmail.com>
2010-12-27 13:25:37 -0600

    merging from dru

--- product/dropkick/Configuration/Dsl/Topshelf/TopshelfConfigurator.cs
+++ product/dropkick/Configuration/Dsl/Topshelf/TopshelfConfigurator.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿// Copyright 2007-2010 The Apache Software Foundation.
 // 
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
@@ -14,6 +13,7 @@
 namespace dropkick.Configuration.Dsl.Topshelf
 {
     using DeploymentModel;
+    using FileSystem;
     using Tasks;
     using Tasks.Topshelf;
 
@@ -21,11 +21,17 @@ namespace dropkick.Configuration.Dsl.Topshelf
         BaseProtoTask,
         TopshelfOptions
     {
+        readonly Path _path;
         string _instanceName;
         string _location;
         string _exeName;
         string _password;
         string _username;
+
+        public TopshelfConfigurator(Path path)
+        {
+            _path = path;
+        }
 
         public void ExeName(string name)
         {
@@ -60,69 +66,4 @@ namespace dropkick.Configuration.Dsl.Topshelf
             }
         }
     }
-=======
-﻿// Copyright 2007-2010 The Apache Software Foundation.
-// 
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
-// this file except in compliance with the License. You may obtain a copy of the 
-// License at 
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0 
-// 
-// Unless required by applicable law or agreed to in writing, software distributed 
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
-// specific language governing permissions and limitations under the License.
-namespace dropkick.Configuration.Dsl.Topshelf
-{
-    using DeploymentModel;
-    using FileSystem;
-    using Tasks;
-    using Tasks.Topshelf;
-
-    public class TopshelfConfigurator :
-        BaseProtoTask,
-        TopshelfOptions
-    {
-        readonly Path _path;
-        string _instanceName;
-        string _location;
-        string _exeName;
-
-        public TopshelfConfigurator(Path path)
-        {
-            _path = path;
-        }
-
-        public void ExeName(string name)
-        {
-            _exeName = name;
-        }
-
-        public void Instance(string name)
-        {
-            _instanceName = name;
-        }
-
-        public void LocatedAt(string location)
-        {
-            _location = location;
-        }
-
-        public override void RegisterRealTasks(PhysicalServer site)
-        {
-            var loc = _location;
-            loc = _path.ConvertUncShareToLocalPath(site, _location);
-
-            if (site.IsLocal)
-            {
-                site.AddTask(new LocalTopshelfTask(_exeName, loc, _instanceName));
-            }
-            else
-            {
-                site.AddTask(new RemoteTopshelfTask(_exeName, loc, _instanceName, site));
-            }
-        }
-    }
->>>>>>> adding path support to topshelf
 }
