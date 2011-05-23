7e67e77abd426729ea207ec24a098fe8ec09988f
Rob Reynolds <ferventcoder@gmail.com>
2011-05-23 15:11:49 -0500

    moving to 0.3.8. Adding a default deployment with the dll

---
+++ nuget/content/DeploymentSettings.cs.pp
@@ -0,0 +1,53 @@
+﻿// ==============================================================================
+// 
+// ACuriousMind and FerventCoder Copyright © 2011 - Released under the Apache 2.0 License
+// 
+// Copyright 2007-2008 The Apache Software Foundation.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+//
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+// ==============================================================================
+using dropkick.Configuration;
+using dropkick.Tasks.RoundhousE;
+using dropkick.Wmi;
+
+namespace $rootnamespace$
+{
+    public class DeploymentSettings : DropkickConfiguration
+    {
+        
+        #region Properties
+
+        //directories
+        public string WebsitePath { get; set; }
+        public string HostServicePath { get; set; }
+
+        //service info
+        public ServiceStartMode ServiceStartMode { get; set; }
+        public string ServiceUserName { get; set; }
+        public string ServiceUserPassword { get; set; }
+
+        //web stuff
+        public string WebUserName { get; set; }
+        public string WebUserPassword { get; set; }
+        public string VirtualDirectorySite { get; set; }
+        public string VirtualDirectoryName { get; set; }
+        
+        //database stuff 
+        public string DbName { get; set; }
+        public string DbSqlFilesPath { get; set; }
+        public string DbRestorePath { get; set; }
+        public DatabaseRecoveryMode DbRecoveryMode { get; set; }
+        public RoundhousEMode RoundhousEMode { get; set; }
+
+        #endregion
+    }
+}
---
+++ nuget/content/TheDeployment.cs.pp
@@ -0,0 +1,124 @@
+﻿// ==============================================================================
+// 
+// ACuriousMind and FerventCoder Copyright © 2011 - Released under the Apache 2.0 License
+// 
+// Copyright 2007-2008 The Apache Software Foundation.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+//
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+// ==============================================================================
+using System.IO;
+using dropkick.Configuration.Dsl;
+using dropkick.Configuration.Dsl.Files;
+using dropkick.Configuration.Dsl.Iis;
+using dropkick.Configuration.Dsl.RoundhousE;
+using dropkick.Configuration.Dsl.Security;
+using dropkick.Configuration.Dsl.WinService;
+
+namespace $rootnamespace$
+{
+    public class TheDeployment : Deployment<TheDeployment, DeploymentSettings>
+    {
+        #region Constructors
+
+        public TheDeployment()
+        {
+            Define(settings =>
+            {
+                DeploymentStepsFor(Db,
+                                   s =>
+                                   {
+                                       s.RoundhousE()
+                                           .ForEnvironment(settings.Environment)
+                                           .OnDatabase(settings.DbName)
+                                           .WithScriptsFolder(settings.DbSqlFilesPath)
+                                           .WithDatabaseRecoveryMode(settings.DbRecoveryMode)
+                                           .WithRestorePath(settings.DbRestorePath)
+                                           .WithRepositoryPath("__REPLACE_ME__")
+                                           .WithVersionFile("_BuildInfo.xml")
+                                           .WithRoundhousEMode(settings.RoundhousEMode)
+                                           ;
+                                   });
+
+                DeploymentStepsFor(Web,
+                                   s =>
+                                   {
+                                       s.CopyDirectory(@"..\_PublishedWebSites\__REPLACE_ME__").To(@"{{WebsitePath}}").DeleteDestinationBeforeDeploying();
+
+                                       s.CopyFile(@"..\environment.files\{{Environment}}\{{Environment}}.web.config").ToDirectory(@"{{WebsitePath}}").RenameTo(@"web.config");
+
+                                       s.Security(securityOptions =>
+                                       {
+                                           securityOptions.ForPath(settings.WebsitePath, fileSecurityConfig => fileSecurityConfig.GrantRead(settings.WebUserName));
+                                           securityOptions.ForPath(Path.Combine(settings.HostServicePath, "logs"), fs => fs.GrantReadWrite(settings.WebUserName));
+                                           securityOptions.ForPath(@"~\C$\Windows\Microsoft.NET\Framework\v2.0.50727\Temporary ASP.NET Files", fs => fs.GrantReadWrite(settings.WebUserName));
+                                           if (Directory.Exists(@"~\C$\Windows\Microsoft.NET\Framework64\v2.0.50727\Temporary ASP.NET Files"))
+                                           {
+                                               securityOptions.ForPath(@"~\C$\Windows\Microsoft.NET\Framework64\v2.0.50727\Temporary ASP.NET Files", fs => fs.GrantReadWrite(settings.WebUserName));
+                                           }
+                                       });
+                                   });
+
+                DeploymentStepsFor(VirtualDirectory,
+                                   s =>
+                                   {
+                                       s.Iis7Site(settings.VirtualDirectorySite).VirtualDirectory(settings.VirtualDirectoryName).SetAppPoolTo("__REPLACE_ME__",
+                                                                                                                                              pool =>
+                                                                                                                                              {
+                                                                                                                                                  pool.SetRuntimeToV4();
+                                                                                                                                                  //pool.UseClassicPipeline();
+                                                                                                                                                  //pool.Enable32BitAppOnWin64();
+                                                                                                                                              }).SetPathTo(@"{{WebsitePath}}");
+                                   });
+
+                DeploymentStepsFor(Host,
+                                   s =>
+                                   {
+                                       var serviceName = "__REPLACE_ME__.{{Environment}}";
+                                       s.WinService(serviceName).Stop();
+
+                                       s.CopyDirectory(@"..\_PublishedApplications\__REPLACE_ME__").To(@"{{HostServicePath}}").DeleteDestinationBeforeDeploying();
+
+                                       s.CopyFile(@"..\environment.files\{{Environment}}\{{Environment}}.__REPLACE_ME__.exe.config").ToDirectory(@"{{HostServicePath}}").RenameTo(@"__REPLACE_ME__.exe.config");
+
+                                       s.Security(o =>
+                                       {
+                                           o.LocalPolicy(lp =>
+                                           {
+                                               lp.LogOnAsService(settings.ServiceUserName);
+                                               lp.LogOnAsBatch(settings.ServiceUserName);
+                                           });
+
+                                           o.ForPath(settings.HostServicePath, fs => fs.GrantRead(settings.ServiceUserName));
+                                           o.ForPath(Path.Combine(settings.HostServicePath,"logs"), fs => fs.GrantReadWrite(settings.ServiceUserName));
+                                       });
+                                       s.WinService(serviceName).Delete();
+                                       s.WinService(serviceName).Create().WithCredentials(settings.ServiceUserName, settings.ServiceUserPassword).WithDisplayName("__REPLACE_ME__ ({{Environment}})").WithServicePath(@"{{HostServicePath}}\__REPLACE_ME__.exe").
+                                           WithStartMode(settings.ServiceStartMode)
+                                           //.AddDependency("MSMQ")
+                                           ;
+                                   });
+            });
+        }
+
+        #endregion
+
+        #region Properties
+
+        //order is important
+        public static Role Db { get; set; }
+        public static Role Web { get; set; }
+        public static Role VirtualDirectory { get; set; }
+        public static Role Host { get; set; }
+
+        #endregion
+    }
+}
