8e03bb39ee549e0652b28aa7be698438644515fd
Rob Reynolds <ferventcoder@gmail.com>
2011-06-13 14:11:41 -0500

    undoing the notetaking from the logger since it will already show up in the logger when running

--- product/dropkick/Tasks/RoundhousE/DeploymentLogger.cs
+++ product/dropkick/Tasks/RoundhousE/DeploymentLogger.cs
@@ -20,7 +20,7 @@ namespace dropkick.Tasks.RoundhousE
 
         public void log_an_info_event_containing(string message, params object[] args)
         {
-            _results.AddNote(message, args);
+           // _results.AddNote(message, args);
         }
 
         public void log_a_warning_event_containing(string message, params object[] args)
