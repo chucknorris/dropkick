8a7d0c767c2e36d88516e335b3525a109cc31e14
Jason Duffett <github@jasonduffett.org>
2011-07-13 12:00:21 +0100

    Fix cascading registry sub key creation

--- product/dropkick/Configuration/Dsl/Registry/ProtoCreateRegistryKeyTask.cs
+++ product/dropkick/Configuration/Dsl/Registry/ProtoCreateRegistryKeyTask.cs
@@ -13,7 +13,7 @@ namespace dropkick.Configuration.Dsl.Registry
 	{
 		private readonly RegistryHive _hive;
 		private readonly string _name;
-		private readonly IList<string> _subKeys = new List<string>();
+		private readonly IList<ProtoCreateRegistryKeyTask> _subKeys = new List<ProtoCreateRegistryKeyTask>();
 		private readonly IList<RegistryValue> _values = new List<RegistryValue>();
 
 		public ProtoCreateRegistryKeyTask(RegistryHive hive, string name)
@@ -23,28 +23,37 @@ namespace dropkick.Configuration.Dsl.Registry
 			_name = ReplaceTokens(name);
 		}
 
+		/// <summary>
+		/// Initializes a new instance of the ProtoCreateRegistryKeyTask class as a sub-key of the given parent key.
+		/// </summary>
+		private ProtoCreateRegistryKeyTask(ProtoCreateRegistryKeyTask parentKey, string subkey)
+		{
+			_hive = parentKey._hive;
+			_name = System.IO.Path.Combine(parentKey._name, ReplaceTokens(subkey));
+		}
+
+		public RegistryKeyOptions CreateRegistryKey(string subKey)
+		{
+			var task = new ProtoCreateRegistryKeyTask(this, subKey);
+			_subKeys.Add(task);
+			return task;
+		}
+
+		public RegistryKeyOptions CreateRegistryKey(string subKey, Action<RegistryKeyOptions> options)
+		{
+			var task = CreateRegistryKey(subKey);
+			if (options != null)
+				options(task);
+			return task;
+		}
+
 		public override void RegisterRealTasks(PhysicalServer server)
 		{
 			server.AddTask(new CreateRegistryKeyTask(server.Name, _hive, _name));
-			_subKeys.Each(subKey =>
-			              server.AddTask(new CreateRegistryKeyTask(server.Name, _hive, System.IO.Path.Combine(_name, subKey))));
+			_subKeys.Each(subKey => subKey.RegisterRealTasks(server));
 			_values.Each(value =>
 			             server.AddTask(new CreateRegistryValueTask(server.Name, _hive, _name, value.Name, value.ValueType,
 			                                                    value.Value)));
-		}
-
-		public RegistryKeyOptions CreateRegistryKey(string name)
-		{
-			_subKeys.Add(name);
-			return this;
-		}
-
-		public RegistryKeyOptions CreateRegistryKey(string name, Action<RegistryKeyOptions> options)
-		{
-			_subKeys.Add(name);
-			if (options != null)
-				options(this);
-			return this;
 		}
 
 		public void CreateValue(string name, RegistryValueKind valueType, object value)
