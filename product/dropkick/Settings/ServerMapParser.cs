8810719f3df32394234c2f705f96d9029f166549
Jason Duffett <github@jasonduffett.org>
2011-07-14 15:00:45 +0100

    Added support to servermaps for specifying an array of servers for a a role. eg. { MyRole: ['server1','server2'] }

--- product/dropkick/Settings/ServerMapParser.cs
+++ product/dropkick/Settings/ServerMapParser.cs
@@ -10,6 +10,8 @@
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
+using System.Linq;
+
 namespace dropkick.Settings
 {
     using System.Collections.Generic;
@@ -26,26 +28,31 @@ namespace dropkick.Settings
         {
             var binder = ConfigurationBinderFactory.New(c =>
             {
-                //c.AddJsonFile(file.FullName);
                 var content = File.ReadAllText(file.FullName);
                 c.AddJson(content);
             });
 
 
-            var d = binder.GetAll();
+        	var d = binder.GetAll();
 
             var result = new RoleToServerMap();
 
-
-
             foreach (var kvp in d)
             {
-                result.AddMap(kvp.Key, kvp.Value.ToString());
+                result.AddMap(
+					stripIndexerFromKey(kvp.Key), 
+					kvp.Value.ToString());
             }
-                
-           
-
             return result;
         }
+
+		/// <summary>
+		/// This is a hack as I couldn't work out how to get Magnum's JsonValueProvider to give me the keys I wanted.
+		/// It will return the key string without the "[0]" indexer.
+		/// </summary>
+		private static string stripIndexerFromKey(string key)
+		{
+			return new string(key.TakeWhile(c => c != '[').ToArray());
+		}
     }
 }
