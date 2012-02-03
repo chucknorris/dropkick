07e043ac133466303c380ea98e57818bb03b3f94
Jason Duffett <github@jasonduffett.org>
2012-02-03 17:57:47 +0000

    Example deployment of an IIS 7 website with http & https bindings

---
+++ product/dropkick.tests/TestObjects/IisHttpsSiteDeploy.cs
@@ -0,0 +1,27 @@
+﻿namespace dropkick.tests.TestObjects
+{
+    using dropkick.Configuration.Dsl;
+    using dropkick.Configuration.Dsl.Iis;
+
+    /// <summary>
+    /// dk.exe execute /deployment:"dropkick.tests.TestObjects.IisHttpsSiteDeploy,dropkick.tests" 
+    /// </summary>
+    public class IisHttpsSiteDeploy :
+        Deployment<IisHttpsSiteDeploy, SampleConfiguration>
+    {
+        public IisHttpsSiteDeploy()
+        {
+            Define(settings =>
+                       DeploymentStepsFor(Web, server =>
+                                                   server.Iis7Site("_Dropkick_6550", @"C:\Temp", bindings =>
+                                                       {
+                                                           bindings.Add("http", 16030);
+                                                           bindings.Add("https", 16031, "13d8ae4000e8d5ac8930c3cdb6c995640c715b86");
+                                                       })
+                                                   .VirtualDirectory("dk_test")
+                                                   .SetPathTo(@"C:\Temp")));
+        }
+
+        public static Role Web { get; set; }
+    }
+}
