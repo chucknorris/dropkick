50e22fa23c4b61099e423cc5c86fbf616595a91b
AkosLukacs <AkosLukacs42@gmail.com>
2012-03-24 15:25:34 +0100

    Extended XmlPokeTask with a 'SetOrInsert' option: Currently 'Set' only updates existing xml elements, but doesn't create it if the element does not exist.
    Use case: add arbitrary xml elements to a third party web application after installing it.
    Created a test that adds Elmah configuration to an empty web.config to demonstrate an "non-basic" usage.

--- product/dropkick/Configuration/Dsl/Xml/ProtoXmlPokeTask.cs
+++ product/dropkick/Configuration/Dsl/Xml/ProtoXmlPokeTask.cs
@@ -25,6 +25,7 @@ namespace dropkick.Configuration.Dsl.Xml
         private readonly string _filePath;
         private IDictionary<string, string> _namespacePrefixes; 
         private IDictionary<string, string> _items = new Dictionary<string, string>();
+        private IDictionary<string, string> _setOrInsertItems = new Dictionary<string, string>();
 
         public ProtoXmlPokeTask(string filePath)
             : this(filePath, null)
@@ -53,11 +54,24 @@ namespace dropkick.Configuration.Dsl.Xml
             return this;
         }
 
+        public XmlPokeOptions SetOrInsert(string xPath, string value) {
+           xPath = ReplaceTokens(xPath);
+           value = ReplaceTokens(value);
+
+           if(_setOrInsertItems.ContainsKey(xPath)) {
+              _setOrInsertItems[xPath] = value;
+           } else {
+              _setOrInsertItems.Add(xPath, value);
+           }
+
+           return this;
+        }
+
         public override void RegisterRealTasks(PhysicalServer site)
         {
             string filePath = site.MapPath(_filePath);
 
-            var o = new XmlPokeTask(filePath, _items, new DotNetPath(), _namespacePrefixes);
+            var o = new XmlPokeTask(filePath, _items, _setOrInsertItems, new DotNetPath(), _namespacePrefixes);
             site.AddTask(o);
         }
     }
