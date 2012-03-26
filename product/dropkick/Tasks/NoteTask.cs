1bb7d8f6ba4df11b538d3da7b421b0208eec639a
AkosLukacs <AkosLukacs42@gmail.com>
2012-03-26 13:57:07 +0200

    The output of a "Note" task (IMO) should show up in all logs.
    For example to add some structure to the output...
    Also added options to explicitly add notes of all kinds of DeploymentItemStatus.

--- product/dropkick/Tasks/NoteTask.cs
+++ product/dropkick/Tasks/NoteTask.cs
@@ -1,31 +1,52 @@
-namespace dropkick.Tasks
-{
-    using DeploymentModel;
+namespace dropkick.Tasks {
+   using DeploymentModel;
 
-    public class NoteTask :
-        Task
-    {
-        readonly string _message;
+   public class NoteTask :
+       Task {
+      readonly DeploymentItemStatus _status;
+      readonly string _message;
 
-        public NoteTask(string message)
-        {
-            _message = message;
-        }
+      public NoteTask(string message, DeploymentItemStatus status) {
+         _message = message;
+         _status = status;
+      }
+      public NoteTask(string message):this(message, DeploymentItemStatus.Note) {}
 
-        public string Name
-        {
-            get { return "NOTE: {0}".FormatWith(_message); }
-        }
+      public string Name {
+         get { return "NOTE: {0}".FormatWith(_message); }
+      }
 
-        public DeploymentResult VerifyCanRun()
-        {
-            return new DeploymentResult();
-        }
+      public DeploymentResult VerifyCanRun() {
+         return ReturnResult();
+      }
 
-        public DeploymentResult Execute()
-        {
-            //do nothing
-            return new DeploymentResult();
-        }
-    }
+      public DeploymentResult Execute() {
+         return ReturnResult();
+      }
+
+      private DeploymentResult ReturnResult() {
+         var dr = new DeploymentResult();
+         switch(_status) {
+            case DeploymentItemStatus.Good:
+               dr.AddGood(_message);
+               break;
+            case DeploymentItemStatus.Alert:
+               dr.AddAlert(_message);
+               break;
+            case DeploymentItemStatus.Error:
+               dr.AddError(_message);
+               break;
+            case DeploymentItemStatus.Note:
+               dr.AddNote(_message);
+               break;
+            case DeploymentItemStatus.Verbose:
+               dr.AddVerbose(_message);
+               break;
+            default:
+               dr.AddError(string.Format(@"Unkown DeploymentItemStatus: '{0}'; message: '{1}'", _status, _message));
+               break;
+         }
+         return dr;
+      }
+   }
 }
