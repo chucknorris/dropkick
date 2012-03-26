842d4b3abebb017ec04818674107f307ac36e93f
AkosLukacs <AkosLukacs42@gmail.com>
2012-03-26 13:15:14 +0200

    Forgot to add note when there is task to do...

--- product/dropkick/Tasks/Files/ExistsTasks.cs
+++ product/dropkick/Tasks/Files/ExistsTasks.cs
@@ -27,37 +27,45 @@
          var result = new DeploymentResult();
 
          if(_filesShouldExist.Count > 0) {
+            bool fErr = false;
             foreach(var filePath in _filesShouldExist) {
                string actualPath = _path.GetFullPath(filePath);
-               if(!_path.IsFile(filePath)) { result.AddError("'{0}' is not an acceptable path. It doesn't appear to be a file.".FormatWith(filePath)); }
+               if(!_path.IsFile(filePath)) { fErr = true; result.AddError("'{0}' is not an acceptable path. It doesn't appear to be a file.".FormatWith(filePath)); }
             }
+            if(!fErr) { result.AddNote("{0} files that should exist.".FormatWith(_filesShouldExist.Count)); }
          } else {
             result.AddNote("No Files that should exist.");
          }
 
          if(_directoriesShouldExist.Count > 0) {
+            bool dErr = false;
             foreach(var dirPath in _directoriesShouldExist) {
                string actualPath = _path.GetFullPath(dirPath);
-               if(!_path.IsDirectory(dirPath)) { result.AddError("'{0}' is not an acceptable path. It doesn't appear to be a directory.".FormatWith(dirPath)); }
+               if(!_path.IsDirectory(dirPath)) { dErr = true; result.AddError("'{0}' is not an acceptable path. It doesn't appear to be a directory.".FormatWith(dirPath)); }
             }
+            if(!dErr) { result.AddNote("{0} directories that should exist.".FormatWith(_directoriesShouldExist.Count)); }
          } else {
             result.AddNote("No Directories that should exist.");
          }
 
          if(_filesShould_NOT_Exist.Count > 0) {
+            bool fnErr = false;
             foreach(var filePath in _filesShould_NOT_Exist) {
                string actualPath = _path.GetFullPath(filePath);
-               if(!_path.IsFile(filePath)) { result.AddError("'{0}' is not an acceptable path. It doesn't appear to be a file.".FormatWith(filePath)); }
+               if(!_path.IsFile(filePath)) { fnErr = true; result.AddError("'{0}' is not an acceptable path. It doesn't appear to be a file.".FormatWith(filePath)); }
             }
+            if(!fnErr) { result.AddNote("{0} files that should not exist.".FormatWith(_filesShould_NOT_Exist.Count)); }
          } else {
             result.AddNote("No Files that should not exist.");
          }
 
          if(_directoriesShould_NOT_Exist.Count > 0) {
+            bool dnErr = false;
             foreach(var dirPath in _directoriesShould_NOT_Exist) {
                string actualPath = _path.GetFullPath(dirPath);
-               if(!_path.IsDirectory(dirPath)) { result.AddError("'{0}' is not an acceptable path. It doesn't appear to be a directory.".FormatWith(dirPath)); }
+               if(!_path.IsDirectory(dirPath)) { dnErr = true; result.AddError("'{0}' is not an acceptable path. It doesn't appear to be a directory.".FormatWith(dirPath)); }
             }
+            if(!dnErr) { result.AddNote("{0} directories that should not exist.".FormatWith(_directoriesShould_NOT_Exist.Count)); }
          } else {
             result.AddNote("No Directories that should not exist.");
          }
