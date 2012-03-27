b1b1683d2ecfc24cbb66c5c8f1872c611bd44c82
AkosLukacs <AkosLukacs42@gmail.com>
2012-03-27 18:18:41 +0200

    Added abort option to DeploymentResult. ExistsTasks can check if a list of directories, and files exists, or does not exist. Also a demonstration for ShouldAbort.

--- product/dropkick.tests/Tasks/Files/ExistsTests.cs
+++ product/dropkick.tests/Tasks/Files/ExistsTests.cs
@@ -6,77 +6,107 @@ using dropkick.FileSystem;
 using dropkick.Tasks.Files;
 using NUnit.Framework;
 using Path = System.IO.Path;
+using dropkick.DeploymentModel;
 
 namespace dropkick.tests.Tasks.Files {
 
    [TestFixture]
    [Category("Integration")]
    public class ExistsTests {
+      string tempFile = Path.GetTempFileName();
+      //these two directories should exist without creating anything...
+      string tempPath = Path.GetTempPath();
+      string dllPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
 
       [Test]
       public void FilesShouldExistTests() {
-         string tempFile = Path.GetTempFileName();
-
          //this exists
-         var fileDoesExist = new ExistsTask(new DotNetPath(), new List<string> { tempFile }, null, null, null);
+         var fileDoesExist = new ExistsTask(new DotNetPath(), "FilesShouldExistTests", new List<string> { tempFile }, null, null, null);
          var result = fileDoesExist.Execute();
          result.ContainsError().ShouldBeFalse();
 
          //this does not.
-         var fileDoesNotExist = new ExistsTask(new DotNetPath(), new List<string> { tempFile + Guid.NewGuid() + ".thisShouldNotExist" }, null, null, null);
+         var fileDoesNotExist = new ExistsTask(new DotNetPath(), "FilesShouldExistTests", new List<string> { tempFile + Guid.NewGuid() + ".thisShouldNotExist" }, null, null, null);
          var resultNotExists = fileDoesNotExist.Execute();
          resultNotExists.ContainsError().ShouldBeTrue();
       }
 
       [Test]
       public void FilesShould_NOT_ExistTests() {
-         string tempFile = Path.GetTempFileName();
-
          //this exists
-         var fileDoesExist = new ExistsTask(new DotNetPath(), null, null, new List<string> { tempFile }, null);
+         var fileDoesExist = new ExistsTask(new DotNetPath(), "FilesShould_NOT_ExistTests", null, null, new List<string> { tempFile }, null);
          var result = fileDoesExist.Execute();
          result.ContainsError().ShouldBeTrue();
 
          //this does not. - result should contain errors!
-         var fileDoesNotExist = new ExistsTask(new DotNetPath(), null, null, new List<string> { tempFile + Guid.NewGuid() + ".thisShouldNotExist" }, null);
+         var fileDoesNotExist = new ExistsTask(new DotNetPath(), "FilesShould_NOT_ExistTests", null, null, new List<string> { tempFile + Guid.NewGuid() + ".thisShouldNotExist" }, null);
          var resultNotExists = fileDoesNotExist.Execute();
          resultNotExists.ContainsError().ShouldBeFalse();
       }
 
       [Test]
       public void DirectoryShouldExistTests() {
-         string tempFile = Path.GetTempPath();
-
          //this exists
-         var fileDoesExist = new ExistsTask(new DotNetPath(), null, new List<string> { tempFile }, null, null);
-         var result = fileDoesExist.Execute();
+         var dirDoesExist = new ExistsTask(new DotNetPath(), "DirectoryShouldExistTests", null, new List<string> { tempPath, dllPath }, null, null);
+         var result = dirDoesExist.Execute();
          result.ContainsError().ShouldBeFalse();
+         result.ShouldContain(DeploymentItemStatus.Good, 1);
 
          //this does not.
-         var fileDoesNotExist = new ExistsTask(new DotNetPath(), null, new List<string> { tempFile + Guid.NewGuid() + ".thisShouldNotExist", tempFile + Guid.NewGuid() + ".thisShouldNotExistToo" }, null, null);
-         var resultNotExists = fileDoesNotExist.Execute();
+         var dirDoesNotExist = new ExistsTask(new DotNetPath(), "DirectoryShouldExistTests", null, new List<string> { tempPath + Guid.NewGuid() + ".thisShouldNotExist", tempPath + Guid.NewGuid() + ".thisShouldNotExistToo" }, null, null);
+         var resultNotExists = dirDoesNotExist.Execute();
          //should contain an error
          resultNotExists.ContainsError().ShouldBeTrue();
          //should be two errors
-         resultNotExists.Results.Count(x => x.Status == dropkick.DeploymentModel.DeploymentItemStatus.Error).ShouldBeEqualTo(2);
+         resultNotExists.ShouldContain(DeploymentItemStatus.Error, 2);
       }
 
       [Test]
       public void DirectoryShould_NOT_ExistTests() {
-         string tempFile = Path.GetTempPath();
-
-         //this exists
-         var fileDoesExist = new ExistsTask(new DotNetPath(), null, null, null, new List<string> { tempFile });
-         var result = fileDoesExist.Execute();
-         result.ContainsError().ShouldBeTrue();
+         //these don't exist, a good thing now
+         var dirDoesExist = new ExistsTask(new DotNetPath(), "DirectoryShould_NOT_ExistTests", null, null, null, new List<string> { tempPath + Guid.NewGuid(), dllPath + Guid.NewGuid() });
+         var result = dirDoesExist.Execute();
+         result.ContainsError().ShouldBeFalse();
+         result.ShouldContain(DeploymentItemStatus.Good, 1);
+         result.ShouldContain(DeploymentItemStatus.Note, 3);
 
          //this does not.
-         var fileDoesNotExist = new ExistsTask(new DotNetPath(), null, null, null, new List<string> { tempFile + Guid.NewGuid() + ".thisShouldNotExist", tempFile + Guid.NewGuid() + ".thisShouldNotExistToo" });
-         var resultNotExists = fileDoesNotExist.Execute();
+         var dirDoesNotExist = new ExistsTask(new DotNetPath(), "DirectoryShould_NOT_ExistTests", null, null, null, new List<string> { tempPath, dllPath });
+         var resultNotExists = dirDoesNotExist.Execute();
          //should contain an error
-         resultNotExists.ContainsError().ShouldBeFalse();
-         //should be two Good results
-         resultNotExists.Results.Count(x => x.Status == dropkick.DeploymentModel.DeploymentItemStatus.Good).ShouldBeEqualTo(2);
+         resultNotExists.ContainsError().ShouldBeTrue();
+         //should be two Error results
+         resultNotExists.ShouldContain(DeploymentItemStatus.Error, 2);
+      }
+
+      [Test]
+      public void ShouldAbortOnError_is_set_it_should_not_abort_when_no_error_found() {
+         var dirDoesExist_valid = new ExistsTask(new DotNetPath(), "dummy", null, new List<string> { tempPath, dllPath }, null, null,
+            true);
+         var result = dirDoesExist_valid.Execute();
+
+         result.ContainsError().ShouldBeFalse();
+         result.ShouldAbort.ShouldBeFalse();
+      }
+
+      [Test]
+      public void ShouldAbortOnError_is_set_it_should_abort_when_error_found() {
+         var dirDoesExist_invalid = new ExistsTask(new DotNetPath(), "dummy", null, new List<string> { tempPath + Guid.NewGuid(), dllPath + Guid.NewGuid() }, null, null,
+            true);
+         var result = dirDoesExist_invalid.Execute();
+
+         result.ContainsError().ShouldBeTrue();
+         result.ShouldAbort.ShouldBeTrue();
+      }
+
+      [Test]
+      public void ShouldAbortOnError_not_set_should_not_abort_when_error_found() {
+         var dirDoesExist_invalid = new ExistsTask(new DotNetPath(), "dummy", null, new List<string> { tempPath + Guid.NewGuid(), dllPath + Guid.NewGuid() }, null, null,
+            false);
+         var result = dirDoesExist_invalid.Execute();
+
+         result.ContainsError().ShouldBeTrue();
+         result.ShouldAbort.ShouldBeFalse();
       }
    }
 }
