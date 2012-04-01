3d1ab4012eef42a2777e08fe33aab246a97ac666
AkosLukacs <AkosLukacs42@gmail.com>
2012-04-02 01:05:01 +0200

    UnzipArchive -> It's now possible to don't delete a folder, but overwrite files.
    ExistsTests.cs contained two errors...

--- product/dropkick/Configuration/Dsl/Files/ProtoUnzipArchiveTask.cs
+++ product/dropkick/Configuration/Dsl/Files/ProtoUnzipArchiveTask.cs
@@ -6,37 +6,43 @@ using Magnum;
 
 namespace dropkick.Configuration.Dsl.Files
 {
-	public class ProtoUnzipArchiveTask : BaseProtoTask, UnzipOptions
-	{
-		readonly Path _path;
-		readonly string _archiveFilename;
-		string _to;
-		DestinationCleanOptions _options = DestinationCleanOptions.None;
+   public class ProtoUnzipArchiveTask : BaseProtoTask, UnzipOptions
+   {
+      readonly Path _path;
+      readonly string _archiveFilename;
+      string _to;
+      DestinationCleanOptions _options = DestinationCleanOptions.None;
+      ExtractExistingFileAction? _explicitExistingFileAction = null;
 
-		public ProtoUnzipArchiveTask(Path path, string archiveFilename)
-		{
-			Guard.AgainstEmpty(archiveFilename);
-			_path = path;
-			_archiveFilename = ReplaceTokens(archiveFilename);
-		}
+      public ProtoUnzipArchiveTask(Path path, string archiveFilename)
+      {
+         Guard.AgainstEmpty(archiveFilename);
+         _path = path;
+         _archiveFilename = ReplaceTokens(archiveFilename);
+      }
 
-		public UnzipOptions To(string destinationPath)
-		{
-			_to = ReplaceTokens(destinationPath);
-			return this;
-		}
+      public UnzipOptions To(string destinationPath)
+      {
+         _to = ReplaceTokens(destinationPath);
+         return this;
+      }
 
-		public void DeleteDestinationBeforeDeploying()
-		{
-			_options = DestinationCleanOptions.Delete;
-		}
+      public UnzipOptions ExistingFilesAction(ExtractExistingFileAction action)
+      {
+         _explicitExistingFileAction = action;
+         return this;
+      }
+      public void DeleteDestinationBeforeDeploying()
+      {
+         _options = DestinationCleanOptions.Delete;
+      }
 
-		public override void RegisterRealTasks(PhysicalServer server)
-		{
-			var to = server.MapPath(_to);
-			var archiveFilename = server.MapPath(_archiveFilename);
-			var task = new UnzipArchiveTask(archiveFilename, to, _options, _path);
-			server.AddTask(task);
-		}
-	}
+      public override void RegisterRealTasks(PhysicalServer server)
+      {
+         var to = server.MapPath(_to);
+         var archiveFilename = server.MapPath(_archiveFilename);
+         var task = new UnzipArchiveTask(archiveFilename, to, _options, _path, _explicitExistingFileAction);
+         server.AddTask(task);
+      }
+   }
 }
--- product/dropkick/Configuration/Dsl/Files/UnzipOptions.cs
+++ product/dropkick/Configuration/Dsl/Files/UnzipOptions.cs
@@ -1,8 +1,20 @@
 ﻿namespace dropkick.Configuration.Dsl.Files
 {
-	public interface UnzipOptions
-	{
-		UnzipOptions To(string destinationPath);
-		void DeleteDestinationBeforeDeploying();
-	}
+   public interface UnzipOptions
+   {
+      UnzipOptions To(string destinationPath);
+
+      /// <summary>
+      /// What to do, if an existing file found.
+      /// Don't use with DeleteDestinationBeforeDeploying
+      /// </summary>
+      /// <param name="action"></param>
+      /// <returns></returns>
+      UnzipOptions ExistingFilesAction(dropkick.Tasks.Files.ExtractExistingFileAction action);
+
+      /// <summary>
+      /// Don't use with ExistingFilesAction
+      /// </summary>
+      void DeleteDestinationBeforeDeploying();
+   }
 }
