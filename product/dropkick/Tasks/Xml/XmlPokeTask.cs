2c0b78dbad87475017fda13d83cc3c526f56e9cd
AkosLukacs <AkosLukacs42@gmail.com>
2012-04-01 23:35:15 +0200

    XmlPoke - Extended the UpdateOrInsertValueInFile with "shouldBeFirst" parameter. If it's true, the new element will be inserted as the first child. Doesn't move existing elements though. Reason: "configSections" element must be the first in a web.config... Test: Xml/XmlPokeTaskSpecs.cs -> "when_inserting_into_the_settings_in_an_xml_file_using_shouldBeFirst"
    note: API at XmlPokeTask level is pretty ugly, because replaced a Dictionary<string, string> with a Dictionary<string, <Tuple<string, bool>>, but that does not affect the fluent API, since adding a new overload to XmlPokeOptions.SetOrInsert covers the internals.
    
    Modified the when_adding_Elmah_to_web_config_file test, because it updated all <location> parts in web.config, even if it was present in the source for a completely different pat. Also configSections is set to be first.

--- product/dropkick/Tasks/Xml/XmlPokeTask.cs
+++ product/dropkick/Tasks/Xml/XmlPokeTask.cs
@@ -27,16 +27,16 @@ namespace dropkick.Tasks.Xml
 
         private string _filePath;
         private readonly IDictionary<string, string> _replacementItems;
-        private readonly IDictionary<string, string> _replaceOrInsertItems;
+        private readonly IDictionary<string, Tuple<string, bool>> _replaceOrInsertItems;
         private readonly IDictionary<string, string> _namespacePrefixes;
 
         public XmlPokeTask(string filePath, IDictionary<string, string> replacementItems, Path path)
-           : this(filePath, replacementItems, new Dictionary<string, string>(), path, new Dictionary<string, string>()) {
+           : this(filePath, replacementItems, new Dictionary<string, Tuple<string, bool>>(), path, new Dictionary<string, string>()) {
         }
         public XmlPokeTask(string filePath, IDictionary<string, string> replacementItems, Path path, IDictionary<string, string> namespacePrefixes)
-           : this(filePath, replacementItems, new Dictionary<string, string>(), path, namespacePrefixes) {
+           : this(filePath, replacementItems, new Dictionary<string, Tuple<string, bool>>(), path, namespacePrefixes) {
         }
-        public XmlPokeTask(string filePath, IDictionary<string, string> replacementItems, IDictionary<string, string> replaceOrInsertItems, Path path)
+        public XmlPokeTask(string filePath, IDictionary<string, string> replacementItems, IDictionary<string, Tuple<string, bool>> replaceOrInsertItems, Path path)
            : this(filePath, replacementItems, replaceOrInsertItems, path, new Dictionary<string, string>()) {
         }
 
@@ -48,7 +48,7 @@ namespace dropkick.Tasks.Xml
         /// <param name="replaceOrInsertItems">these items will be replace or added if not present in the source xml</param>
         /// <param name="path"></param>
         /// <param name="namespacePrefixes"></param>
-        public XmlPokeTask(string filePath, IDictionary<string, string> replacementItems, IDictionary<string, string> replaceOrInsertItems, Path path, IDictionary<string, string> namespacePrefixes)
+        public XmlPokeTask(string filePath, IDictionary<string, string> replacementItems, IDictionary<string, Tuple<string, bool>> replaceOrInsertItems, Path path, IDictionary<string, string> namespacePrefixes)
             : base(path)
         {
             _filePath = filePath;
@@ -87,12 +87,8 @@ namespace dropkick.Tasks.Xml
             return result;
         }
 
-        private void UpdateXmlFile(DeploymentResult result, string filePath, IDictionary<string, string> replacementItems,IDictionary<string, string> insertItems, IDictionary<string, string> namespacePrefixes)
+        private void UpdateXmlFile(DeploymentResult result, string filePath, IDictionary<string, string> replacementItems, IDictionary<string, Tuple<string, bool>> insertItems, IDictionary<string, string> namespacePrefixes)
         {
-            //XmlTextReader xmlReader = new XmlTextReader(_filePath);
-            //XPathDocument xmlDoc = new XPathDocument(xmlReader);
-            //XPathExpression expression = xpathNavigator.Compile(xPath);
-
             LogFileChange("[xmlpoke] Starting changes to '{0}'.", filePath);
 
             XmlDocument document = new XmlDocument();
@@ -109,8 +105,8 @@ namespace dropkick.Tasks.Xml
                 UpdateValueInFile(result, xpathNavigator, item.Key, item.Value, nsManager);
             }
 
-            foreach(KeyValuePair<string, string> item in insertItems.OrEmptyListIfNull()) {
-               UpdateOrInsertValueInFile(result,document, xpathNavigator, item.Key, item.Value, nsManager);
+            foreach(var item in insertItems.OrEmptyListIfNull()) {
+               UpdateOrInsertValueInFile(result,document, xpathNavigator, item.Key, item.Value.Item1, item.Value.Item2, nsManager);
             } 
 
             LogFileChange("[xmlpoke] Completed changes to '{0}'.",filePath);
@@ -118,17 +114,24 @@ namespace dropkick.Tasks.Xml
             document.Save(filePath);
         }
 
-        private void UpdateOrInsertValueInFile(DeploymentResult result, XmlDocument document, XPathNavigator xpathNavigator, string xPath, string value, XmlNamespaceManager nsManager) {
-           var selected = xpathNavigator.Select(xPath, nsManager);
-           if(selected.Count > 0) {
-              foreach(XPathNavigator navigator in selected) {
-                 string original = navigator.Value;
-                 navigator.SetValue(value);
-                 LogFileChange("[xmlpoke] Updated '{0}' to '{1}'.", original, value);
+        private void UpdateOrInsertValueInFile(DeploymentResult result, XmlDocument document, XPathNavigator xpathNavigator, string xPath, string value,bool shouldBeFirst, XmlNamespaceManager nsManager)
+        {
+           try {
+              var selected = xpathNavigator.Select(xPath, nsManager);
+              if(selected.Count > 0) {
+                 foreach(XPathNavigator navigator in selected) {
+                    string original = navigator.Value;
+                    navigator.SetValue(value);
+                    LogFileChange("[xmlpoke] Updated '{0}' to '{1}'.", original, value);
+                 }
+              } else {
+                 Set(document, xPath, value, shouldBeFirst, nsManager);
+                 LogFileChange("[xmlpoke] Inserting '{0}' to xPath '{1}'.", value, xPath);
               }
-           } else {
-              Set(document, xPath, value, nsManager);
-              LogFileChange("[xmlpoke] Inserting '{0}' to xPath '{1}'.", value, xPath);
+           }
+           catch(Exception exc) {
+              //to make debuggin easier...
+              throw new Exception(exc.Message + " xPath: '" + xPath + "'", exc);
            }
         }
 
@@ -139,7 +142,7 @@ namespace dropkick.Tasks.Xml
        /// <param name="xpath"></param>
        /// <param name="value"></param>
        /// <param name="nsManager"></param>
-        static void Set(XmlDocument doc, string xpath, string value, XmlNamespaceManager nsManager) {
+        static void Set(XmlDocument doc, string xpath, string value, bool shouldBeFirst, XmlNamespaceManager nsManager) {
            if(doc == null)
               throw new ArgumentNullException("doc");
            if(string.IsNullOrEmpty(xpath))
@@ -149,13 +152,20 @@ namespace dropkick.Tasks.Xml
            if(nodes.Count > 1) {
               throw new XPathException("Xpath '" + xpath + "' was not found multiple times!");
            } else if(nodes.Count == 0) {
-              createXPath(doc, xpath, nsManager).InnerText = value;
+              createXPath(doc, xpath, nsManager, shouldBeFirst).InnerText = value;
            } else {
               nodes[0].InnerText = value;
            }
         }
-
-        static XmlNode createXPath(XmlDocument doc, string xpath, XmlNamespaceManager nsManager) {
+       /// <summary>
+       /// 
+       /// </summary>
+       /// <param name="doc"></param>
+       /// <param name="xpath"></param>
+       /// <param name="nsManager"></param>
+        /// <param name="shouldBeFirst">insert it as the first child of the parent node. Reason: @lt;configSections@gt; element must be the first in a web.config...</param>
+       /// <returns></returns>
+        static XmlNode createXPath(XmlDocument doc, string xpath, XmlNamespaceManager nsManager, bool shouldBeFirst) {
            XmlNode node = doc;
            //mod: trim off all leading slashes
            foreach(string part in xpath.TrimStart('/', '\\').Split('/')) {
@@ -186,11 +196,15 @@ namespace dropkick.Tasks.Xml
                     string nsLong = nsManager.LookupNamespace(nsShort);
 
                     XmlNode next = doc.CreateElement(elementName, nsLong);
-                    node.AppendChild(next);
+                    if(shouldBeFirst) {
+                       node.InsertBefore(next, node.FirstChild);
+                    } else { node.AppendChild(next); }
                     node = next;
                  } else {
                     XmlNode next = doc.CreateElement(elName);
-                    node.AppendChild(next);
+                    if(shouldBeFirst) {
+                       node.InsertBefore(next, node.FirstChild);
+                    } else { node.AppendChild(next); }
                     node = next;
                  }
 
